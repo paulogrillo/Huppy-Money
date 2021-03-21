8f9f7211a18b32d6c0df183a9dcd278664d395e0
Paulo <paulodigitalart@gmail.com>
2021-03-21 16:01:50 -0300

    static-final

--- src/components/Summary/index.tsx
+++ src/components/Summary/index.tsx
@@ -15,7 +15,7 @@ export function Summary(){
                  <p>Entradas</p>
                  <img src={incomeImg} alt="Entradas"/>
              </header>
-             <strong> R$50,00</strong>
+             <strong> R$59.850,08</strong>
          </div>
 
          <div>
@@ -31,7 +31,7 @@ export function Summary(){
                  <p>Total</p>
                  <img src={totalImg} alt="total"/>
              </header>
-             <strong>R$ 100,00</strong>
+             <strong>R$59.800,08</strong>
          </div>
      </Container>
     );
--- src/components/TransactionsTable/index.tsx
+++ src/components/TransactionsTable/index.tsx
@@ -1,11 +1,18 @@
+import { useEffect } from 'react';
 import {Container} from './styles'
+
 export function TransactionsTable(){
+
+    useEffect(() => {
+        fetch('http://locahost:3000/api/transactions')
+    }, []);
+    
     return(
     <Container>
         <table>
         <thead>
         <tr>
-            <th>Tútulo</th>
+            <th>Tútulo da transação</th>
             <th>Valor</th>
             <th>Categoria</th>
             <th>Data</th>
@@ -15,16 +22,38 @@ export function TransactionsTable(){
 
         <tbody>
             <tr>
-                <td>Desenvolvimento de website</td>
-                <td className="deposit">R$ 12.00</td>
-                <td>Desenvolvimento</td>
+                <td>Salário</td>
+                <td className="deposit">R$ 12.700</td>
+                <td>Trabalho</td>
                 <td>20/02/21</td>
             </tr>
 
             <tr>
                 <td>Aluguel</td>
-                <td className="widthdraw">-R$ 20.00</td>
+                <td className="widthdraw">-R$ 2.850</td>
                 <td>Casa</td>
+                <td>18/03/21</td>
+            </tr>
+
+            <tr>
+                <td>Freela</td>
+                <td className="deposit">R$ 1.500</td>
+                <td>Marketing</td>
+                <td>20/02/21</td>
+            </tr>
+
+
+            <tr>
+                <td>Compras</td>
+                <td className="widthdraw">-R$ 300.00</td>
+                <td>Mercado</td>
+                <td>20/02/21</td>
+            </tr>
+
+            <tr>
+                <td>Plano de Telefone</td>
+                <td className="widthdraw">-R$ 50.00</td>
+                <td>Outros</td>
                 <td>18/03/21</td>
             </tr>
 
