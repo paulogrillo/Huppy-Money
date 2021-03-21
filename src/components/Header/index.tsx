9816e249a3042a063db1df05b3dd1b0a6373fd5d
Paulo <paulodigitalart@gmail.com>
2021-03-21 13:18:59 -0300

    up-logo

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,4 +1,4 @@
-import logoImg from '../../assets/logo.svg'
+import logoImg from '../../assets/huppy-money.png'
 import { Container, Content } from './styles'
 
 export function Header(){
--- src/components/Header/styles.ts
+++ src/components/Header/styles.ts
@@ -13,6 +13,10 @@ export const Content = styled.div `
     align-items: center;
     justify-content: space-between;
 
+img {
+     
+
+ }
  button {
     font-size: 1rem;
     color: #fff;
