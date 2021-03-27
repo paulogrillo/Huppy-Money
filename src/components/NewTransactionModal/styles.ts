3bc5b3de40c0447a646393a117aee805ab1ea306
Paulo <paulodigitalart@gmail.com>
2021-03-27 16:07:48 -0300

    up-button-action

--- src/components/NewTransactionModal/styles.ts
+++ src/components/NewTransactionModal/styles.ts
@@ -74,5 +74,42 @@ input {
 `;
 
 export const TransactionTypeContainer = styled.div`
+    margin: 1rem 0;
+    display: grid;
+    grid-template-columns: 1fr 1fr;
+    gap: 0.5rem;
 
+button{
+    height: 4rem;
+    border: 1px solid #d7d7d7;
+    border-radius: 0.25rem;
+
+    background: transparent;
+
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    outline: none;
+
+    &:hover{
+        border-color: var(--myheader);
+    &:active{
+        border-color: var(--myheader);
+    }
+    }
+
+    img{
+        width: 26px;
+        height: 20px;
+    }
+    span{
+        display: inline-block;
+        margin-left: 1rem;
+
+        font-size: 1rem;
+        color: var(--text-title);
+
+    }
+
+}
 `;
