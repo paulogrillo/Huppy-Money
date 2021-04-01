4c2c256695327355bffccdc59ee224fa9269787d
Paulo <paulodigitalart@gmail.com>
2021-03-31 21:23:05 -0300

    up

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import { GlobalStyle } from "./styles/global";
 
 import Modal from 'react-modal'
 import { NewTransactionModal } from "./components/NewTransactionModal";
+import { TransactionsContext } from "./TransactionsContexts";
 
 Modal.setAppElement('#root');
 
@@ -20,7 +21,7 @@ export function App() {
   }
 
   return (
-    <>
+    <TransactionsContext.Provider value={[]}>
       <Header onOpenNewTransactionModal={handleOpenNewTransactionModal} />
       <Dashboard />
 
@@ -29,6 +30,6 @@ export function App() {
         onRequestClose={handleCloseNewTransactionModal}
       />
       <GlobalStyle />
-    </>
+    </TransactionsContext.Provider>
   );
 }
