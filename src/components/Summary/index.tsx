ff48eed5c0d327392767b57967d738df414b464d
paulogrillo <paulodigitalart@gmail.com>
2021-12-29 22:45:32 -0300

    up

--- src/components/Summary/index.tsx
+++ src/components/Summary/index.tsx
@@ -1,4 +1,4 @@
-import React, {useContext} from 'react'
+import React, {useContext} from 'react';
 import incomeImg from '../../assets/income.svg';
 import outcomeImg from '../../assets/outcome.svg';
 import totalImg from '../../assets/total.svg';
